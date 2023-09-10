2fc834682bdea15fc5458befce6bea7d52296bb3
Francisco <franciscopassarelli7@gmail.com>
2023-09-10 16:46:07 -0300

    api

---
+++ app/about/page.jsx
@@ -0,0 +1,11 @@
+
+function AboutPage() {
+  return (
+    <div>
+        <h1>AboutPage</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam facere pariatur quis doloremque est officiis quos, eaque quas! Neque, odit maxime delectus quam totam debitis magnam nesciunt molestiae mollitia eius.</p>
+        </div>
+  )
+}
+
+export default AboutPage
---
+++ app/services/page.jsx
@@ -0,0 +1,11 @@
+
+function ServicesPage() {
+  return (
+    <div>
+        <h1>Services</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae quisquam doloribus inventore necessitatibus, aspernatur asperiores blanditiis harum dolor consequuntur minima repellat. Minus asperiores ex possimus quia, consequuntur harum recusandae quasi!</p>
+    </div>
+  )
+}
+
+export default ServicesPage
